ac0535a61cd065e93bc240dd6e3f7670077c8418
anjaliinfo <98171488+anjaliinfo@users.noreply.github.com>
2023-09-21 12:30:54 +0530

    Update main.js
    added new es6 features

--- app/main.js
+++ app/main.js
@@ -98,13 +98,13 @@ obj.getName(); // output: The Full-Name is John Smith
 
 // Default Parameters 
 
- const tempFunc = (name,age=33) =>{
+ const tempdFunc = (name,age=33) =>{
 
    return "Hi ${name}, Your age is ${age}! ";
 
  }
 
- console.log(tempFunc('tom'));  
+ console.log(tempdFunc('tom'));  
 
 
  //import and export
@@ -188,10 +188,10 @@ constructor(name) {
       this.name = name;
     }
 }
-let person = new People("Anjali Sh");
-console.log(person.Name);
-person.Name = "Sh";
-console.log(person.Name);  
+let persons = new People("Anjali Sh");
+console.log(persons.Name);
+persons.Name = "Sh";
+console.log(persons.Name);  
 
 //Methods
 
@@ -210,24 +210,24 @@ word.repeat(5) // "Anjali Anjali Anjali Anjali Anjali"
 
 //We can easily determine if something begins a string using startsWith().
 
-const word = 'Disestablishmentarianism'
+const letters = 'Disestablishmentarianism'
 
-word.startsWith('Disestablish') // true 
+letters.startsWith('Disestablish') // true 
 
 //String.prototype.includes()
 
 
-const word = 'Disestablishmentarianism'
+const words = 'Disestablishmentarianism'
 
-word.includes('establishment') // true 
+words.includes('establishment') // true 
 
 
 //String.prototype.endsWith()
 
 
-const word = 'Disestablishmentarianism'
+const strings = 'Disestablishmentarianism'
 
-word.endsWith('ism') // true 
+strings.endsWith('ism') // true 
 
 //Implicit Returns
 
@@ -239,4 +239,128 @@ const returnsTrue = () => true;
 let poemData = `Johny Johny Yes Papa,     
                 Eating sugar?  No, papa!
                 Telling lies? No, papa!
-                Open your mouth Ah, ah, ah!`
+                Open your mouth Ah, ah, ah!`  
+
+
+
+// Blocked Scope Variables
+
+let be = new Array('a', 'b', 'c');
+let ce = new Array('d', 'e', 'f');
+
+let x = new Array('1', '2');
+let y = new Array('3', '4');
+
+for (let i = 0; i < a.length; i++) {
+  let x = a[i];
+  console.log(`Value x[${i}]:`, x);
+}
+for (let i = 0; i < b.length; i++) {
+  let y = b[i];
+  console.log(`Value y[${i}]:`, y);
+}
+
+console.log(`Array x:`, JSON.stringify(x));
+console.log(`Array y:`, JSON.stringify(y));
+
+let callbacks = []
+for (let i = 0; i <= 2; i++) {
+  callbacks[i] = function() {
+    return i * 2;
+  }
+}
+console.log(`Compare callback[0] === 0:`, callbacks[0]() === 0);
+console.log(`Compare callback[1] === 2:`, callbacks[1]() === 2);
+console.log(`Compare callback[2] === 4:`, callbacks[2]() === 4);  
+
+
+//Block-Scoped Functions
+
+function test() {
+  return 1;
+}
+console.log(`Compare test() === 1:`, test() === 1);
+
+{
+  function test() {
+    return 2;
+  }
+  console.log(`Compare test() === 2:`, test() === 2);
+}
+
+console.log(`Compare test() === 1:`, test() === 1);   
+
+
+
+//Expression Bodies
+
+let evens = new Array(1, 2, 3);
+
+let odds = evens.map(v => v + 1);
+let pairs = evens.map(v => ({
+  even: v,
+  odd: v + 1
+}));
+let nums = evens.map((v, i) => v + i);
+
+console.log(`Array odds:`, JSON.stringify(odds));
+console.log(`Array pairs:`, JSON.stringify(pairs));
+console.log(`Array nums:`, JSON.stringify(nums)); 
+
+//Statement Bodies
+
+let numss = new Array(2, 5, 3, 6, 8, 4, 5, 1, 5);
+let fives = new Array();
+
+numss.forEach(v => {
+  if (v % 5 === 0)
+    fives.push(v);
+});
+
+console.log(`Array fives:`, JSON.stringify(fives)); 
+
+//Output Array fives: [5,5,5] 
+
+
+//Raw String Access  
+
+
+function quux(strings, ...values) {
+  console.log(`Compare strings[0] === "foo\\n":`, strings[0] === "foo\n");
+  console.log(`Compare strings[1] === "bar":`, strings[1] === "bar");
+  console.log(`Compare values[0] === 42:`, values[0] === 42);
+  console.log(`Compare strings.raw[0] === "foo\\\\n":`, strings.raw[0] === "foo\\n");
+  console.log(`Compare strings.raw[1] === "bar":`, strings.raw[1] === "bar");
+}
+quux `foo\n${ 42 }bar`;
+
+console.log(`Compare String.war:`, String.raw `foo\n${ 42 }bar` === "foo\\n42bar"); 
+
+//output Compare strings[0] === "foo\n": true
+//Compare strings[1] === "bar": true
+//Compare values[0] === 42: true
+//Compare strings.raw[0] === "foo\\n": true
+//Compare strings.raw[1] === "bar": true
+//Compare String.war: true  
+
+
+//Regular Expression Sticky Matching
+
+
+let message = "My name is Foo and my lastname is Bar."
+let pattern = /my [a-zA-Z]+/y;
+
+console.log(`Return pattern /my [a-zA-Z]+/y:`, JSON.stringify(pattern.exec(message)));
+
+pattern.lastIndex = 19;
+
+console.log(`Return pattern /my [a-zA-Z]+/y:`, JSON.stringify(pattern.exec(message)));
+console.log(`Return pattern /my [a-zA-Z]+/y:`, JSON.stringify(pattern.exec(message))); 
+
+
+//Output -// Return pattern /my [a-zA-Z]+/y: null
+//Return pattern /my [a-zA-Z]+/y: ["my lastname"]
+//Return pattern /my [a-zA-Z]+/y: null
+
+
+
